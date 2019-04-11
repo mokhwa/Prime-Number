9df12b51bbe3af417ec2ef27b9873052b6f396c7
dit05 <dit@dit05ui-imac.local>
2019-04-11 14:09:24 +0900

    Initial Commit

---
+++ Prime Number/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prime Number
+//
+//  Created by dit05 on 2019. 4. 11..
+//  Copyright © 2019년 dit05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
